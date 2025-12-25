e99d469c03eb3f7e4f79e2169063a4039a60d0d0
Shihab5500 <Shihabahmed6501@gmail.com>
2025-12-26 01:45:55 +0600

    some change

--- api/index.js
+++ api/index.js
@@ -4,18 +4,28 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected Successfully!"))
-  .catch((err) => console.log("❌ Mongo Error:", err));
-
+// routes
 const messageRoutes = require("../routes/messageRoutes");
 app.use("/api", messageRoutes);
 
+// test route
+app.get("/", (req, res) => {
+  res.send("API running successfully 🚀");
+});
+
+// MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.log("❌ Mongo Error:", err));
+
+// ⚠️ PORT MUST EXIST
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🔥 Server running on port ${PORT}`);
 });
